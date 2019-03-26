9a0e6c4ba12d4381c3a63f7d6ba1d6e0d47c949a
Michael Pieschel <mpieschel39@gmail.com>
2019-03-25 19:46:10 -0700

    Update Tortoise_racing.rb

--- Ruby/Tortoise_racing.rb
+++ Ruby/Tortoise_racing.rb
@@ -1,12 +1,22 @@
+# def race(v1, v2, g)
+#   result = []
+#   if v1 >= v2
+#     result = nil
+#   else 
+#     time = (g * 6) / ((v2 - v1) * 0.1) * 60
+#     result.push(Time.at(time).utc.strftime("%H").to_i)
+#     result.push(Time.at(time).utc.strftime("%M").to_i)
+#     result.push(Time.at(time).utc.strftime("%S").to_i)
+#   end
+#   result
+# end
+
 def race(v1, v2, g)
   result = []
-  if v1 >= v2
-    result = nil
-  else 
-    time = (g * 6) / ((v2 - v1) * 0.1) * 60
-    result.push(Time.at(time).utc.strftime("%H").to_i)
-    result.push(Time.at(time).utc.strftime("%M").to_i)
-    result.push(Time.at(time).utc.strftime("%S").to_i)
-  end
+  return nil if v1 >= v2
+  time = (g * 3600) / (v2 - v1)
+  result.push(Time.at(time).utc.strftime("%H").to_i)
+  result.push(Time.at(time).utc.strftime("%M").to_i)
+  result.push(Time.at(time).utc.strftime("%S").to_i)
   result
 end
