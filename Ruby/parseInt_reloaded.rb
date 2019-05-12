355ce628669794b6d31852e4d29333b5ab67c31e
Michael Pieschel <mpieschel39@gmail.com>
2019-05-12 10:23:41 -0700

    fix comments

--- Ruby/parseInt_reloaded.rb
+++ Ruby/parseInt_reloaded.rb
@@ -1,17 +1,17 @@
-// In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
+# In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
 
-// Examples:
+# Examples:
 
-// "one" => 1
-// "twenty" => 20
-// "two hundred forty-six" => 246
-// "seven hundred eighty-three thousand nine hundred and nineteen" => 783919
-// Additional Notes:
+# "one" => 1
+# "twenty" => 20
+# "two hundred forty-six" => 246
+# "seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+# Additional Notes:
 
-// The minimum number is "zero" (inclusively)
-// The maximum number, which must be supported is 1 million (inclusively)
-// The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
-// All tested numbers are valid, you don't need to validate them
+# The minimum number is "zero" (inclusively)
+# The maximum number, which must be supported is 1 million (inclusively)
+# The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
+# All tested numbers are valid, you don't need to validate them
 
 def parse_int(string)
   small = {'zero'=> 0, 'one'=> 1, 'two'=> 2, 'three'=> 3, 'four'=> 4, 'five'=> 5,
